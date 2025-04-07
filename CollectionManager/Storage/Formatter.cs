7e38a5a7c36c632744c068a1d96f98330af066bb
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-04-07 23:17:40 +0200

    saving stuff

--- CollectionManager/Storage/Formatter.cs
+++ CollectionManager/Storage/Formatter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CollectionManager.Helpers;
 using CollectionManager.Models;
 
 namespace CollectionManager.Storage
@@ -16,8 +18,18 @@ namespace CollectionManager.Storage
             sb.AppendLine($"Id={collection.Id}");
             sb.AppendLine($"Name={collection.Name}");
             sb.AppendLine($"Description={collection.Description}");
-            sb.AppendLine("[CollectionEnd]");
+            return sb.ToString();
+        }
 
+        public static string SerializeCollections(List<Collection> collections)
+        {
+            StringBuilder sb = new StringBuilder();
+            collections.ForEach(coll =>
+            {
+                sb.AppendLine(SerializeCollection(coll));
+            });
+
+            return sb.ToString();
         }
 
         public static string SerializeCollectionItems(Collection collection, string path)
@@ -33,20 +45,128 @@ namespace CollectionManager.Storage
         public static string SerializeCollectionItem(CollectionItem item)
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine("[Item]");
-            sb.AppendLine($"Id={item.Id}");
-            sb.AppendLine($"Name={item.Name}");
-            sb.AppendLine($"Description={item.Description}");
-            sb.AppendLine($"Price={item.Price}");
-            sb.AppendLine($"CollectionDate={item.CollectionDate}");
-            sb.AppendLine($"SellDate={item.SellDate}");
-            sb.AppendLine($"Rating={item.Rating}");
-            sb.AppendLine($"Sold={item.Sold}");
-            sb.AppendLine($"ToSale={item.ToSale}");
-            sb.AppendLine($"ImageName={item.ImageName}");
-            sb.AppendLine($"CustomVariable={item.customVariable}");
-            sb.AppendLine("[ItemEnd]");
+            sb.Append("[Item]\n");
+            sb.Append($"Id={item.Id}\n");
+            sb.Append($"Name={item.Name}\n");
+            sb.Append($"CollectionName={item.CollectionName}\n");
+            sb.Append($"Description={item.Description}\n");
+            sb.Append($"Price={item.Price}\n");
+            sb.Append($"CollectionDate={item.CollectionDate}\n");
+            sb.Append($"SellDate={item.SellDate}\n");
+            sb.Append($"Rating={item.Rating}\n");
+            sb.Append($"Sold={item.Sold}\n");
+            sb.Append($"ToSale={item.ToSale}\n");
+            sb.Append($"ImageName={item.ImageName}\n");
+            sb.Append($"CustomVariable={item.customVariable}\n");
             return sb.ToString();
+        }
+
+        public static List<CollectionItem> DeserializeItems(string data)
+        {
+            List<CollectionItem> parsedItems = new List<CollectionItem>();
+            List<string> items = data.Split(new string[] { "[Item]" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            items.ForEach(item =>
+            {
+                item = item.Replace("[Item]", "");
+                List<string> attributes = item.ReplaceLineEndings().Split("\r\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                Dictionary<string, string> attributesSplit = new Dictionary<string, string>();
+                for (int i = 0; i < attributes.Count; i++)
+                {
+                    int index = attributes[i].IndexOf("=");
+                    attributesSplit.Add(attributes[i].Substring(0, index), attributes[i].Substring(index + 1));
+                }
+
+                CollectionItem newItem = new CollectionItem();
+                foreach (KeyValuePair<string, string> entry in attributesSplit)
+                {
+                    string key = entry.Key;
+                    string value = entry.Value;
+
+                    Debug.WriteLine($"{key}: {value}");
+
+                    switch (key)
+                    {
+                        case "Id":
+                            newItem.Id = int.Parse(value);
+                            break;
+                        case "Name":
+                            newItem.Name = value;
+                            break;
+                        case "Description":
+                            newItem.Description = value;
+                            break;
+                        case "Price":
+                            newItem.Price = float.Parse(value);
+                            break;
+                        case "CollectionDate":
+                            newItem.CollectionDate = DateTime.Parse(value);
+                            break;
+                        case "SellDate":
+                            newItem.SellDate = DateTime.Parse(value);
+                            break;
+                        case "Rating":
+                            newItem.Rating = int.Parse(value);
+                            break;
+                        case "Sold":
+                            newItem.Sold = bool.Parse(value);
+                            break;
+                        case "ToSale":
+                            newItem.ToSale = bool.Parse(value);
+                            break;
+                        case "ImageName":
+                            newItem.ImageName = value;
+                            break;
+                        case "CustomVariable":
+                            newItem.customVariable = CustomVariable.Parse(value);
+                            break;
+                        case "CollectionName":
+                            newItem.CollectionName = value;
+                            break;
+                    }
+                }
+                parsedItems.Add(newItem);
+            });
+            return parsedItems;
+        }
+
+        public static List<Collection> DeserializeCollections(string data)
+        {
+            List<Collection> parsedCollections = new List<Collection>();
+            List<string> collections = data.Split(new string[] { "[Collection]" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            collections.ForEach(collection =>
+            {
+                collection = collection.Replace("[Collection]", "");
+                List<string> attributes = collection.ReplaceLineEndings().Split("\r\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+                Dictionary<string, string> attributesSplit = new Dictionary<string, string>();
+                for (int i = 0; i < attributes.Count; i++)
+                {
+                    int index = attributes[i].IndexOf("=");
+                    attributesSplit.Add(attributes[i].Substring(0, index), attributes[i].Substring(index + 1));
+                }
+                Collection newCollection = new Collection();
+                foreach (KeyValuePair<string, string> entry in attributesSplit)
+                {
+                    string key = entry.Key;
+                    string value = entry.Value;
+                    Debug.WriteLine($"{key}: {value}");
+                    switch (key)
+                    {
+                        case "Id":
+                            newCollection.Id = int.Parse(value);
+                            break;
+                        case "Name":
+                            newCollection.Name = value;
+                            break;
+                        case "Description":
+                            newCollection.Description = value;
+                            break;
+                    }
+                }
+                parsedCollections.Add(newCollection);
+            });
+            return parsedCollections;
         }
     }
 }
--- CollectionManager/Storage/Manager.cs
+++ CollectionManager/Storage/Manager.cs
@@ -5,29 +5,73 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CollectionManager.Storage.Formatter;
 
 namespace CollectionManager.Storage
 {
     static class Manager
     {
-        private static string StoragePath = Path.Combine(FileSystem.AppDataDirectory, "/data");
-        public static void Initialize()
+        private static string StoragePath = Path.Combine(FileSystem.AppDataDirectory, "data");
+        public static void InitializeStorage()
         {
-            Debug.WriteLine("Storage path: " + StoragePath);
+            Debug.WriteLine("Storage path: " + $"{FileSystem.AppDataDirectory.ToString()}\\data");
             if (!Directory.Exists(StoragePath))
             {
                 Directory.CreateDirectory(StoragePath);
             }
         }
 
-        public static string SaveItem(CollectionItem item)
+        public static void SaveItem(CollectionItem item)
         {
             using (StreamWriter sw = new StreamWriter(Path.Combine(StoragePath, item.CollectionName + ".txt"), true))
             {
-                sw.Write(Formatter.SerializeCollectionItem(item));
+                sw.Write(SerializeCollectionItem(item));
             }
         }
 
+        //public static string SaveItems(List<CollectionItem> items)
+        //{
+        //    using (StreamWriter sw = new StreamWriter(Path.Combine(StoragePath, item.CollectionName + ".txt"), true))
+        //    {
+        //        sw.Write(Formatter.SerializeCollectionItems(items));
+        //    }
+        //}
 
+        public static List<CollectionItem> LoadItems(string collectionName)
+        {
+            using (StreamReader sr = new StreamReader(Path.Combine(StoragePath, collectionName + ".txt")))
+            {
+                string data = String.Empty;
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    data += line + "\r\n";
+                }
+                return DeserializeItems(data);
+            }
+        }
+
+        public static void SaveCollection(Collection collection)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(StoragePath, "collections.txt"), true))
+            {
+                sw.Write(SerializeCollection(collection));
+            }
+        }
+
+        public static List<Collection> LoadCollections()
+        {
+            using(StreamReader sr = new StreamReader(Path.Combine(StoragePath, "collections.txt")))
+            {
+                string data = String.Empty;
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    data += line + "\r\n";
+                }
+                return DeserializeCollections(data);
+            }
+
+        }
     }
 }
