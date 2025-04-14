c5f34c292665eaf035231ab062e4930b88d3739d
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-04-15 00:19:03 +0200

    change content based on what is displayed

--- CollectionManager/CustomControls/InfoBar.xaml.cs
+++ CollectionManager/CustomControls/InfoBar.xaml.cs
@@ -2,8 +2,22 @@ namespace CollectionManager.CustomControls;
 
 public partial class InfoBar : ContentView
 {
-	public InfoBar()
-	{
-		InitializeComponent();
-	}
+    public InfoBar()
+    {
+        InitializeComponent();
+    }
+
+    public void SetContent(string page)
+    {
+        if (page == "CollectionList")
+        {
+            CollectionsInfo.IsVisible = true;
+            CollectionInfo.IsVisible = false;
+        }
+        else if (page == "CollectionItemList")
+        {
+            CollectionsInfo.IsVisible = false;
+            CollectionInfo.IsVisible = true;
+        }
+    }
 }
