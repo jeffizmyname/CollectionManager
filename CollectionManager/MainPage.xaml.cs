feeb5678a24d50e24e553110ef136c2b41b35dcc
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-04-15 00:20:21 +0200

    magic of communication and manimation

--- CollectionManager/MainPage.xaml.cs
+++ CollectionManager/MainPage.xaml.cs
@@ -12,17 +12,56 @@ namespace CollectionManager
         public MainPage()
         {
             InitializeComponent();
-            CollectionListPage.newCollectionButtonClicked_Listener += Swap;
-            FormPage.createCollectionButtonClicked_Listener +=
+            CollectionListPage.newButtonClicked_Listener += (e, data) => Swap(data);
+            CollectionListPage.pageChanged_Listener += (e, data) => Reset(data);
+            FormCollectionPage.createCollectionButtonClicked_Listener += (s, data) => CollectionListPage._collectionViewModel.AddCollection(new Collection(data[0], data[1]));
         }
 
-        async void Swap(object sender, EventArgs e)
+        async void Reset(string data)
+        {
+            if (SidePanel.Content != null)
+            {
+                await SidePanel.Content.TranslateTo(SidePanel.Width + 100, 0, 500);
+
+                if (data == "CollectionList")
+                {
+                    InfoBarPage.IsVisible = true;
+                    FormCollectionPage.IsVisible = false;
+                    FormItemPage.IsVisible = false;
+                }
+                else if (data == "CollectionItemList")
+                {
+                    InfoBarPage.IsVisible = true;
+                    FormCollectionPage.IsVisible = false;
+                    FormItemPage.IsVisible = false;
+                }
+                InfoBarPage.SetContent(data);
+
+                await SidePanel.Content.TranslateTo(0, 0, 500);
+                return;
+            }
+        }
+
+        async void Swap(string data)
         {
             if(SidePanel.Content != null)
             {
-                await SidePanel.Content.TranslateTo(SidePanel.Width+100, 0, 500);
-                InfoBarPage.IsVisible = false;
-                FormPage.IsVisible = true;
+                await SidePanel.Content.TranslateTo(SidePanel.Width + 100, 0, 500);
+
+                if (data == "CollectionList")
+                {
+                    InfoBarPage.IsVisible = false;
+                    FormCollectionPage.IsVisible = true;
+                    FormItemPage.IsVisible = false;
+                }
+                else if(data == "CollectionItemList")
+                {
+                    InfoBarPage.IsVisible = false;
+                    FormCollectionPage.IsVisible = false;
+                    FormItemPage.IsVisible = true;
+                }
+                
+
                 await SidePanel.Content.TranslateTo(0, 0, 500);
                 return;
             }
