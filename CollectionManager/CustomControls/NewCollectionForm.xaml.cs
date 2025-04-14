1314bd1cd6ee70177f8be2a32ce60f1377df1bec
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-04-15 00:19:31 +0200

    items view model

--- CollectionManager/CustomControls/NewCollectionForm.xaml.cs
+++ CollectionManager/CustomControls/NewCollectionForm.xaml.cs
@@ -2,18 +2,20 @@ namespace CollectionManager.CustomControls;
 
 public partial class NewCollectionForm : ContentView
 {
-    public event EventHandler<string>? createCollectionButtonClicked_Listener;
+    public event EventHandler<List<string>>? createCollectionButtonClicked_Listener;
 
     public NewCollectionForm()
 	{
 		InitializeComponent();
 	}
 
-    private void CreateCollectionButton_Clicked(object sender, EventArgs e)
+    public void CreateCollectionButton_Clicked(object sender, EventArgs e)
     {
 		if(!string.IsNullOrEmpty(CollectionNameEntry.Text) && !string.IsNullOrEmpty(CollectionDescriptionEntry.Text))
 		{
             createCollectionButtonClicked_Listener.Invoke(sender, new List<string> { CollectionNameEntry.Text, CollectionDescriptionEntry.Text });
+            CollectionNameEntry.Text = string.Empty;
+            CollectionDescriptionEntry.Text = string.Empty;
         }
     }
 }
