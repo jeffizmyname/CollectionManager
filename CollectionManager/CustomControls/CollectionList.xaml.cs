537adb7265bb773ba04a48b3a831e1757310074b
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-04-15 00:18:47 +0200

    something

--- CollectionManager/CustomControls/CollectionList.xaml.cs
+++ CollectionManager/CustomControls/CollectionList.xaml.cs
@@ -1,18 +1,25 @@
 using CollectionManager.Models;
 using CollectionManager.ViewModels;
+using Microsoft.Maui.Controls.Handlers.Items;
 
 
 namespace CollectionManager.CustomControls;
 
 public partial class CollectionList : ContentView
 {
-    public event EventHandler? newCollectionButtonClicked_Listener;
+    public event EventHandler<string>? newButtonClicked_Listener;
+    public event EventHandler<string>? pageChanged_Listener;
     public CollectionViewModel? _collectionViewModel { get; set; }
+    public CollectionItemViewModel? _collectionItemViewModel { get; set; }
+
+    public string currentPage { get; set; } = "CollectionList";
     public CollectionList()
     {
         InitializeComponent();
         _collectionViewModel = new CollectionViewModel();
+        _collectionItemViewModel = new CollectionItemViewModel();
         CollectionListView.BindingContext = _collectionViewModel;
+        CollectionItemsListView.BindingContext = _collectionItemViewModel;
     }
 
 
@@ -23,17 +30,63 @@ public partial class CollectionList : ContentView
             Collection selectedItem = e.CurrentSelection[0] as Collection;
             if (selectedItem != null)
             {
-                await TitleLabel.TranslateTo(0, -100, 500);
+                await Task.WhenAll(
+                    TitleLabel.TranslateTo(0, -100, 500),
+                    BackButton.TranslateTo(0, -100, 500)
+                );
+
                 TitleLabel.Text = selectedItem.Name;
-                await TitleLabel.TranslateTo(0, 0, 2000);
-                //Navigation.PushAsync(new CollectionDetailPage(selectedItem));
+
+                AddNewButton.Text = "New Item";
+
+                _collectionItemViewModel.SetCollection(selectedItem.Name);
+
+                BackButton.IsVisible = true;
+                CollectionListView.IsVisible = false;
+                CollectionItemsListView.IsVisible = true;
+
+                
+
+                currentPage = "CollectionItemList";
+                pageChanged_Listener.Invoke(e, currentPage);
+
+                await Task.WhenAll(
+                    TitleLabel.TranslateTo(0, 0, 2000),
+                    BackButton.TranslateTo(0, 0, 2000)
+                );
+
+
             }
+            ((CollectionView)sender).SelectedItem = null;
         }
 
     }
 
-    private void NewCollectionButton_Clicked(object sender, EventArgs e)
+    private void AddNewButton_Clicked(object sender, EventArgs e)
     {
-        newCollectionButtonClicked_Listener.Invoke(e, e);
+        newButtonClicked_Listener.Invoke(e, currentPage);
+    }
+
+    private async void BackButton_Tapped(object sender, TappedEventArgs e)
+    {
+        await Task.WhenAll(
+            TitleLabel.TranslateTo(0, -100, 500),
+            BackButton.TranslateTo(0, -100, 500)
+        );
+
+        TitleLabel.Text = "Your Collections";
+        AddNewButton.Text = "New Collection";
+
+        BackButton.IsVisible = false;
+        CollectionListView.IsVisible = true;
+        CollectionItemsListView.IsVisible = false;
+
+        currentPage = "CollectionList";
+        pageChanged_Listener.Invoke(e, currentPage);
+
+        await Task.WhenAll(
+            TitleLabel.TranslateTo(0, 0, 2000),
+            BackButton.TranslateTo(0, 0, 2000)
+        );
     }
 }
